0d95ef0ebc702024f5e235a8c401061e1e79d78a
Nicolas <nkao552@gmail.com>
2018-04-13 20:08:07 -0700

    landingPage Bootstraped

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import LandingPage from './components/landingPage/landingPage'
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <LandingPage />
+      </div>
+    );
+  }
+}
+
+export default App;
